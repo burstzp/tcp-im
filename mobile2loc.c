13bffed8fd90bb4c388655b96a76bb69347582c5
yiguopan <yiguopan@126.com>
2014-09-23 12:18:48 +0800

    Update mobile2loc.c

--- mobile2loc.c
+++ mobile2loc.c
@@ -12,7 +12,7 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author: guopan1<guopan1@staff.sina.com.cn>                                                             |
+  | Author: yiguopan<yiguopan@126.com>                                                             |
   +----------------------------------------------------------------------+
 */
 
